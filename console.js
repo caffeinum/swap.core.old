369cf02dab18114aa136eb43dd192e44ddf195f0
Aleksey Bykhun <theisease@gmail.com>
2018-05-17 17:54:57 +0300

    use keys

--- console.js
+++ console.js
@@ -2,32 +2,41 @@ const { setupEnv, SwapApp } = require('./lib')
 
 const Ipfs = require('ipfs')
 const IpfsRoom = require('ipfs-pubsub-room')
-const bitcoinJs = require('bitcoinjs-lib')
-const web3 = require('web3')
+const bitcoin = require('bitcoinjs-lib')
+const Web3 = require('web3')
+
+const web3 = new Web3(new Web3.providers.HttpProvider('https://rinkeby.infura.io/JCnK5ifEPH9qcQkX0Ahl'))
 
 const LocalStorage = require('node-localstorage').LocalStorage
 const localStorage = new LocalStorage('./storage')
 
-const env = { Ipfs, IpfsRoom, bitcoinJs, web3, localStorage }
+
+const ethPrivateKey = localStorage.getItem('ethPrivateKey') || ethPrivKey
+const btcPrivateKey = localStorage.getItem('btcPrivateKey') || btcPrivKey
+
+const eth = web3.eth.accounts.wallet.add(ethPrivKey)
+const btc = new bitcoin.ECPair.fromWIF(btcPrivKey, bitcoin.networks.testnet)
+
+const env = { Ipfs, IpfsRoom, bitcoinJs: bitcoin, web3, localStorage }
 setupEnv(env)
 
-const app = new SwapApp({
+const app = global.app = new SwapApp({
   me: {
-    reputation: 10,
+    reputation: 33,
     eth: {
-     address: '0x0',
-     publicKey: '0x0',
+      address:    eth.address,
+      publicKey:  eth.publicKey,
     },
     btc: {
-     address: '0x0',
-     publicKey: '0x0',
+      address:    btc.getAddress(),
+      publicKey:  btc.getPublicKeyBuffer().toString('hex'),
     },
   },
   config: {
     ipfs: {
       swarm: [
         '/dns4/star.wpmix.net/tcp/443/wss/p2p-websocket-star',
-        '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star'
+        // '/dns4/ws-star.discovery.libp2p.io/tcp/443/wss/p2p-websocket-star',
       ]
     }
   }
@@ -35,6 +44,9 @@ const app = new SwapApp({
 
 app.on('ready', () => {
   console.log('swapApp ready')
+
+  console.log('btc', btc.getAddress())
+  console.log('eth', eth.address)
 
   main()
 })
@@ -47,16 +59,26 @@ app.on('user offline', (peer) => {
   console.log('user offline', peer)
 })
 
-app.on('new swap', (swap) => {
-  console.log('new swap', swap)
+app.on('new orders', (swaps) => {
+  console.log('new orders', swaps)
 })
 
-app.on('new swap request', ({ swapId, participant }) => {
+app.on('new order', (swap) => {
+  console.log('new order', swap)
+})
+
+app.on('remove order', (swap) => {
+  console.log('remove order', swap)
+})
+
+app.on('new order request', ({ swapId, participant }) => {
   console.error(`user ${participant.peer} requesting swap`, {
-    swap: app.swapCollection.getByKey(swapId),
+    swap: app.orderCollection.getByKey(swapId),
     participant,
   })
 })
+
+setInterval( () => main(), 5000 )
 
 function main() {
   let swaps = app.getOrders()
@@ -74,6 +96,6 @@ function main() {
   //
   // app.createSwap(0)
 
-  console.log('swaps', app.getOrders())
+  // console.log('swaps', app.getOrders())
 
 }
