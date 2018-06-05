4ad931511918dccd9992220a48a30bd87536aa15
Aleksey Bykhun <theisease@gmail.com>
2018-06-05 19:25:08 +0300

    + go swap

--- server/services/swap.js
+++ server/services/swap.js
@@ -1,33 +1,40 @@
 const { EthSwap, BtcSwap } = require('../../lib/swaps')
 const { ETH2BTC, BTC2ETH, ETHTOKEN2BTC, BTC2ETHTOKEN, } = require('../../lib/flows')
 
-const { bitcoin, ethereum } = require('../services').wallet
+const { wallet } = require('../services')
+const { bitcoin, ethereum } = wallet
+
+const ethSwap = new EthSwap({
+  gasLimit: 3e6,
+})
+
+const btcSwap = new BtcSwap({
+  account: wallet.btc,
+  fetchUnspents: (scriptAddress) => bitcoin.fetchUnspents(scriptAddress),
+  broadcastTx: (txRaw) => bitcoin.broadcastTx(txRaw),
+})
+
+// console.log('eth swap', ethSwap)
+// console.log('btc swap', btcSwap)
+
+const fetchBalanceBtc = () => bitcoin.fetchBalance(wallet.btc.getAddress())
+const fetchBalanceEth = () => ethereum.fetchBalance(wallet.eth.address)
+
+const FLOW_CONFIG = (isBtc) => ({
+  ethSwap,
+  btcSwap,
+  fetchBalance: (isBtc) ? fetchBalanceBtc : fetchBalanceEth,
+})
 
 let _swap
 const start = (swap, wallet) => {
   _swap = swap
 
-  const ethSwap = new EthSwap({
-    gasLimit: 3e6,
-  })
+  const flow = swap.setFlow(BTC2ETH, FLOW_CONFIG)
 
-  const btcSwap = new BtcSwap({
-    account: wallet.btc,
-    fetchUnspents: (scriptAddress) => bitcoin.fetchUnspents(scriptAddress),
-    broadcastTx: (txRaw) => bitcoin.broadcastTx(txRaw),
-  })
-
-  const fetchBalance = () => bitcoin.fetchBalance(wallet.btc.getAddress())
-
-  const flow = swap.setFlow(BTC2ETH, {
-    ethSwap,
-    btcSwap,
-    fetchBalance,
-  })
-
-  swap.flow.on('state update', handleFlowStateUpdate)
-  swap.flow.on('leave step', handleLeaveStep)
-  swap.flow.on('enter step', handleEnterStep)
+  // swap.flow.on('state update', handleFlowStateUpdate)
+  // swap.flow.on('leave step', handleLeaveStep)
+  // swap.flow.on('enter step', handleEnterStep)
 }
 
 const handleFlowStateUpdate = (values) => {
@@ -54,6 +61,9 @@ const submitSecret = (swap, secret) => {
 }
 
 module.exports = {
+  FLOW_CONFIG,
+  flows: { ETH2BTC, BTC2ETH, ETHTOKEN2BTC, BTC2ETHTOKEN, },
+  swaps: { ethSwap, btcSwap, },
   start,
   signSwapFlow,
   submitSecret,
