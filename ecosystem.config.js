490ff5d1e7903dda460924b40c5f3b7073376509
Aleksey Bykhun <theisease@gmail.com>
2018-06-06 15:50:44 +0300

    + pm2 config

---
+++ ecosystem.config.js
@@ -0,0 +1,39 @@
+module.exports = {
+  /**
+   * Application configuration section
+   * http://pm2.keymetrics.io/docs/usage/application-declaration/
+   */
+  apps : [
+    {
+      name            : 'alice',
+      script          : './server/app.js',
+      args            : '1234567890',
+      env             : {
+        PORT : 7777,
+        DEBUG_PORT: 7077,
+      }
+    },
+    {
+      name            : 'bob',
+      script          : './server/app.js',
+      args            : '9876543210',
+      env             : {
+        PORT : 8888,
+        DEBUG_PORT: 8088,
+      }
+    },
+    {
+      name            : 'api',
+      script          : './server/app.js',
+      args            : '',
+      env             : {
+        PORT : 1337,
+      }
+    },
+  ],
+
+  /**
+   * Deployment section
+   * http://pm2.keymetrics.io/docs/usage/deployment/
+   */
+};
