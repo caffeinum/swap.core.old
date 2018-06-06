9ac926705fa422a4c3da5a229a097670b7cc977f
Aleksey Bykhun <theisease@gmail.com>
2018-06-06 18:55:49 +0300

    + withdraw eth

--- server/routes/me/index.js
+++ server/routes/me/index.js
@@ -1,12 +1,13 @@
 const { Router } = require('express')
 const router = new Router()
 
-const { status, balance, getMe, getWallet, listMyOrders } = require('./controller')
+const { status, balance, getMe, getWallet, withdraw, listMyOrders } = require('./controller')
 
 router.get('/', getMe)
 router.get('/status', status)
 router.get('/balance', balance)
 router.get('/wallet', getWallet)
+router.get('/withdraw/:from', withdraw)
 router.get('/orders', listMyOrders)
 
 module.exports = router
